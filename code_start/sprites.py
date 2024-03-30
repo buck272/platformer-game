d51bb303202b128e110398a2e133d06d88c953cd
Ron M <rtm2702@gmail.com>
2024-03-30 12:04:42 +0100

    Updated collidable object, added semi collidable objects functionality, loaded player idle image

--- code_start/sprites.py
+++ code_start/sprites.py
@@ -12,12 +12,18 @@ class MovingSprite(Sprite):
     def __init__(self, groups, start_pos, end_pos, move_dir, speed):
         surf = pygame.Surface((200, 50))
         super().__init__(start_pos, surf, groups)
+        if move_dir == 'x':
+            self.rect.midleft = start_pos
+        else:
+            self.rect.midtop = start_pos
+            
         self.rect.center = start_pos
         self.start_pos = start_pos
         self.end_pos = end_pos
         self.speed = speed
         
         # movement
+        self.moving = True
         self.speed = speed
         self.direction = vector(1, 0) if move_dir == 'x' else vector(0, 1)
         self.move_dir = move_dir
@@ -27,6 +33,16 @@ class MovingSprite(Sprite):
             if self.rect.right >= self.end_pos[0] and self.direction.x == 1:
                 self.direction.x = -1
                 self.rect.right = self.end_pos[0]
+            elif self.rect.left <= self.start_pos[0] and self.direction.x == -1:
+                self.direction.x = 1
+                self.rect.left = self.start_pos[0]
+        elif self.move_dir == 'y':
+            if self.rect.bottom >= self.end_pos[1] and self.direction.y == 1:
+                self.direction.y = -1
+                self.rect.bottom = self.end_pos[1]
+            elif self.rect.top <= self.start_pos[1] and self.direction.y == -1:
+                self.direction.y = 1
+                self.rect.top = self.start_pos[1]
         
     def update(self, dt):
         self.old_rect = self.rect.copy()
