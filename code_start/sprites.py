751e6a598764e292c266eddf3c37984df0adfb37
Ron M <rtm2702@gmail.com>
2024-04-08 09:48:58 +0200

    Added animation to some elements graphics

--- code_start/sprites.py
+++ code_start/sprites.py
@@ -13,6 +13,14 @@ class AnimatedSprite(Sprite):
     def __init__(self, pos, frames, groups, z = Z_LAYERS['main'], animation_speed = ANIMATION_SPEED):
         self.frames, self.frame_index = frames, 0
         super().__init__(pos, self.frames[self.frame_index], groups, z)
+        self.animation_speed = ANIMATION_SPEED
+        
+    def animate(self, dt):
+        self.frame_index += self.animation_speed * dt
+        self.image = self.frames[int(self.frame_index % len(self.frames))]
+    
+    def update(self, dt):
+        self.animate(dt)
         
 class MovingSprite(Sprite):
     def __init__(self, groups, start_pos, end_pos, move_dir, speed):
