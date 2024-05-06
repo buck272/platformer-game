786cbb4e92a5403f8692c482673be9b788e52ab6
Ron M <rtm2702@gmail.com>
2024-05-06 13:00:03 +0200

    Small updates

--- code_start/main.py
+++ code_start/main.py
@@ -32,7 +32,9 @@ class Game():
             'helicopter': import_folder('.', 'graphics', 'level', 'helicopter'),
             'boat': import_folder('.', 'graphics', 'objects', 'boat'),
             'spike': import_image('.', 'graphics', 'enemies', 'spike_ball', 'Spiked Ball'),
-            'spike_chain': import_image('.', 'graphics', 'enemies', 'spike_ball', 'spiked_chain')
+            'spike_chain': import_image('.', 'graphics', 'enemies', 'spike_ball', 'spiked_chain'),
+            'tooth': import_folder('.', 'graphics', 'enemies', 'tooth', 'run'),
+            'shell': import_sub_folders('.', 'graphics', 'enemies', 'shell')
         }
     
     def run(self):
